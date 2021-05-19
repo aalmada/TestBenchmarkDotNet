a9de9320491ea88210f47f03e2d36b50bb8e2d0a
Antao Almada <antao.almada@farfetch.com>
2021-05-19 09:24:36 +0100

    Simplify Program.cs

--- TestBenchmarkDotNet/Program.cs
+++ TestBenchmarkDotNet/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace TestBenchmarkDotNet
 {
@@ -11,24 +6,7 @@ namespace TestBenchmarkDotNet
     {
         static void Main(string[] args)
         {
-            var config = DefaultConfig.Instance
-                .AddDiagnoser(MemoryDiagnoser.Default)
-                .AddJob(Job.Default
-                    .WithRuntime(CoreRuntime.Core50)
-                    .WithId(".NET 5")
-                );
-                // .AddJob(Job.Default
-                //     .WithRuntime(CoreRuntime.Core60)
-                //     .WithEnvironmentVariables(
-                //         new EnvironmentVariable("COMPlus_ReadyToRun", "0"),
-                //         new EnvironmentVariable("COMPlus_TC_QuickJitForLoops", "1"),
-                //         new EnvironmentVariable("COMPlus_TieredPGO", "1"))
-                //     .WithId(".NET 6")
-                // );
-            
-            Console.WriteLine("Executed tests:");
-            foreach (var summary in BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config))
-                Console.WriteLine(summary.Title);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
